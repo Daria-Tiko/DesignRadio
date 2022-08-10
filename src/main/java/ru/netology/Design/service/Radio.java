907389a3f1dc301717c2ad7e9b6879833443f1cd
Darya Tishchenko QA-41 <tdaniel@bk.ru>
2022-08-10 13:04:04 +0400

    Add Radio

--- src/main/java/ru/netology/Design/service/Radio.java
+++ src/main/java/ru/netology/Design/service/Radio.java
@@ -1,4 +1,68 @@
-package ru.netology.Design
+package ru.netology.Design.service;
 
-public class Radio {
-}
+
+    public class Radio {
+        private int currentRadioStation;
+        private int currentVolume;
+
+        public int getCurrentRadioStation() {
+            return currentRadioStation;
+        }
+        public int getCurrentVolume() {
+            return currentVolume;
+        }
+
+        public void setCurrentRadioStation(int newCurrentRadioStation) {
+            if (newCurrentRadioStation < 0) {
+                return;
+            }
+            if (newCurrentRadioStation > 9) {
+                return;
+            }
+            currentRadioStation = newCurrentRadioStation;
+        }
+        public int nextStation(int newCurrentRadioStation) {
+            if (newCurrentRadioStation < 9) {
+                currentRadioStation = newCurrentRadioStation + 1;
+            }
+            if (newCurrentRadioStation >= 9) {
+                currentRadioStation = 0;
+            }
+            return currentRadioStation;
+        }
+        public int prevStation(int newCurrentRadioStation) {
+            if (newCurrentRadioStation > 0) {
+                currentRadioStation = newCurrentRadioStation - 1;
+            }
+            if (newCurrentRadioStation <= 0) {
+                currentRadioStation = 9;
+            }
+            return currentRadioStation;
+        }
+
+        public void setCurrentVolume(int newCurrentVolume) {
+            if (newCurrentVolume >= 0 & newCurrentVolume <= 10) {
+                currentVolume = newCurrentVolume;
+            }
+        }
+        public int volumeUp(int newCurrentVolume) {
+            if (newCurrentVolume < 10) {
+                currentVolume = newCurrentVolume + 1;
+            }
+            if (newCurrentVolume >= 10) {
+                currentVolume = 10;
+            }
+            return currentVolume;
+        }
+        public int volumeDown(int newCurrentVolume) {
+            if (newCurrentVolume > 0) {
+                currentVolume = newCurrentVolume -1;
+            }
+            if (newCurrentVolume <= 0) {
+                currentVolume = 0;
+            }
+            return currentVolume;
+        }
+
+
+    }
--- src/test/java/ru/netology/Design/service/RadioTest.java
+++ src/test/java/ru/netology/Design/service/RadioTest.java
@@ -1,2 +1,94 @@
-package PACKAGE_NAME;public class RadioTest {
+package ru.netology.Design.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+public class RadioTest {
+    Radio station = new Radio();
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/radioStation.csv")
+    public void shouldSetCurrentRadioStation(int newCurrentRadioStation, int expected) {
+
+        station.setCurrentRadioStation(newCurrentRadioStation);
+
+        int actual = station.getCurrentRadioStation();
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "3,4",
+            "9,0",
+            "0,1"
+    })
+    public void shouldSetNextRadioStation(int newCurrentRadioStation, int expected) {
+
+        station.nextStation(newCurrentRadioStation);
+
+        int actual = station.getCurrentRadioStation();
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0,9",
+            "5,4",
+            "9,8"
+    })
+    public void shouldSetPrevRadioStation(int newCurrentRadioStation, int expected) {
+
+        station.prevStation(newCurrentRadioStation);
+
+        int actual = station.getCurrentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Volume.csv")
+    public void shouldSetCurrentVolume(int newCurrentVolume, int expected) {
+
+        station.setCurrentVolume(newCurrentVolume);
+
+        int actual = station.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "5,6",
+            "10,10",
+            "11,10"
+    })
+    public void shouldSetVolumeUp(int newCurrentVolume, int expected) {
+
+        station.volumeUp(newCurrentVolume);
+
+        int actual = station.getCurrentVolume();
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "8,7",
+            "0,0",
+            "-1,0"
+    })
+    public void shouldSetVolumeDown(int newCurrentVolume, int expected) {
+
+        station.volumeDown(newCurrentVolume);
+
+        int actual = station.getCurrentVolume();
+
+        Assertions.assertEquals(expected,actual);
+    }
+
 }
